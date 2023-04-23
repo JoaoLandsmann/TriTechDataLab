dc7c15c7c8e5a982ad694d5f37d8f32b00d97e2d
JVLandsmann <123707517+JVLandsmann@users.noreply.github.com>
2023-04-22 21:58:54 -0300

    Mandei os arquivos atualizados
    Vamos partir daqui a programação gurizada

---
+++ connection.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from flask_mysqldb import MySQL
+
+app = Flask(__name__, static_folder='/Users/jvlan/Desktop/PROJETAO PYTHON/templates')
+
+# configuração do mySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'db_site'
+
+# inicialização do objeto MySQL
+mysql = MySQL(app)
+
+# definição da rota padrão
+@app.route('/')
+def index():
+    # conexão com o banco de dados
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM modulos")
+    data = cur.fetchall()
+    cur.close()
+    return render_template('index.php', data=data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- index.php
+++ index.php
@@ -1,27 +1,43 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Meu site</title>
+	<title>TriTechDataLab</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Conteúdo da tabela modulos:</h1>
-    <table>
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Nome</th>
-                <th>Descrição</th>
-            </tr>
-        </thead>
-        <tbody>
-            {% for row in rows %}
-            <tr>
-                <td>{{ row[0] }}</td>
-                <td>{{ row[1] }}</td>
-                <td>{{ row[2] }}</td>
-            </tr>
-            {% endfor %}
-        </tbody>
-    </table>
+	<header>
+		<h1>Biblioteca</h1>
+		<nav>
+			<ul>
+				<li><a href="#">Início</a></li>
+				<li><a href="#">Catálogo</a></li>
+				<li><a href="#">Minha Conta</a></li>
+				<li><a href="#">Sobre Nós</a></li>
+			</ul>
+		</nav>
+	</header>
+
+	<main>
+		<table>
+			<tr>
+				<th>Id</th>
+				<th>Nome</th>
+				<th>Descrição</th>
+			</tr>
+			{% for row in data %}
+			<tr>
+				<td>{{ row[0] }}</td>
+				<td>{{ row[1] }}</td>
+				<td>{{ row[2] }}</td>
+			</tr>
+			{% endfor %}
+		</table>
+	</main>
+
+	<footer>
+		<p>&copy; 2023 TTDL </p>
+	</footer>
 </body>
 </html>
