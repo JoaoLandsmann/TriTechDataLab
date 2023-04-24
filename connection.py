9ac421a3bdbf13b21aeddbb7cd7bea2d72824569
JuanitoJns <jvvlandsmann@gmail.com>
2023-04-24 14:05:30 -0300

    Pastas organizadas
    Vamos partir daqui. Linkei as páginas url e agr é só programar!

--- connection.py
+++ connection.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from flask_mysqldb import MySQL
 
-app = Flask(__name__, static_folder='D:\Xampp\htdocs\Projeto-Dos-Guri/templates')
+app = Flask(__name__, static_folder='D:\Xampp\htdocs\Projeto-Dos-Guri\templates')
 
 # configuração do mySQL
 app.config['MYSQL_HOST'] = 'localhost'
@@ -12,15 +12,34 @@ app.config['MYSQL_DB'] = 'db_site'
 # inicialização do objeto MySQL
 mysql = MySQL(app)
 
+
 # definição da rota padrão
-@app.route('/')
+@app.route('/index.php')
 def index():
-    # conexão com o banco de dados
+    # fazendo uma consulta nos módulos
     cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM modulos")
     data = cur.fetchall()
     cur.close()
     return render_template('index.php', data=data)
+
+@app.route('/modulos.php')
+def modulos():
+    # fazendo uma consulta nos módulos
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM modulos")
+    data = cur.fetchall()
+    cur.close()
+    return render_template('modulos.php', data=data)
+
+@app.route('/bibliotecas.php')
+def modulos():
+    # fazendo uma consulta nos módulos
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM modulos")
+    data = cur.fetchall()
+    cur.close()
+    return render_template('bibliotecas.php', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
