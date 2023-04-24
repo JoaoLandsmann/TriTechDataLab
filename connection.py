d242f990128573246545992fcf8a7a9a1a3993a5
JVLandsmann <123707517+JVLandsmann@users.noreply.github.com>
2023-04-24 19:56:56 -0300

    Barra de pesquisa
    Adicionei a barra de pesquisa com python puro e mudei os arquivos que eram .php para .html
    
    Vai facilitar a vida :>

--- connection.py
+++ connection.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
 
-app = Flask(__name__, static_folder='D:\Xampp\htdocs\Projeto-Dos-Guri\templates')
+app = Flask(__name__, static_folder='D:\Xampp\htdocs\Projeto-Dos-Guri')
 
 # configuração do mySQL
 app.config['MYSQL_HOST'] = 'localhost'
@@ -12,43 +12,51 @@ app.config['MYSQL_DB'] = 'db_site'
 # inicialização do objeto MySQL
 mysql = MySQL(app)
 
-
 # definição da rota padrão
-@app.route('/index.php')
+@app.route('/index.html')
 def index():
     # fazendo uma consulta nos módulos
     cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM table_index")
     data = cur.fetchall()
     cur.close()
-    return render_template('index.php', data=data)
+    return render_template('index.html', data=data)
 
-@app.route('/modulos.php')
-def modulos():
-    # fazendo uma consulta nos módulos
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM modulos")
-    data = cur.fetchall()
-    cur.close()
-    return render_template('modulos.php', data=data)
-
-@app.route('/bibliotecas.php')
+@app.route('/bibliotecas.html')
 def bibliotecas():
-    # fazendo uma consulta nos módulos
+    # fazendo uma consulta nas bibliotecas
     cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM bibliotecas")
     data = cur.fetchall()
     cur.close()
-    return render_template('bibliotecas.php', data=data)
+    return render_template('bibliotecas.html', data=data)
 
-@app.route('/about.php')
+@app.route('/about.html')
 def about_us():
-    # fazendo uma consulta nos módulos
+    # about the devs
     cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM about_us")
     data = cur.fetchall()
     cur.close()
-    return render_template('about.php', data=data)
+    return render_template('about.html', data=data)
+
+# rota para lidar com a pesquisa
+@app.route('/pesquisa.html', methods=['get'])
+def pesquisa():
+    # recupera o termo de pesquisa do formulário
+    query = request.args.get('query')
+
+    # cria o cursor para executar a consulta
+    cur = mysql.connection.cursor()
+
+    # executa a consulta para pesquisar por um termo em cada uma das tabelas
+    cur.execute("SELECT * FROM bibliotecas WHERE id LIKE %s OR nome LIKE %s OR descricao LIKE %s", (f'%{query}%', f'%{query}%', f'%{query}%'))
+    bibliotecas_results = cur.fetchall()
+
+    cur.close()
+
+    # retorna os resultados da pesquisa
+    return render_template('pesquisa.html', query=query, bibliotecas_results=bibliotecas_results)
 
 if __name__ == '__main__':
     app.run(debug=True)
