e9496da54d7549ac1dd31fd6ceeadfe757aace32
JuanitoJns <129124539+JuanitoJns@users.noreply.github.com>
2023-05-03 16:15:01 -0300

    Delete abluble directory

--- abluble/import pandas.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from sqlalchemy import create_engine
-
-engine = create_engine('mysql+pymysql://root:@localhost:5000/db_site}')
-      
-query = "SELECT * FROM bibliotecas;"
- 
-df2 = pd.read_sql(query, engine)
-print(df2.head())
